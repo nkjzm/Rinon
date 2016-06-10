aa8c7e712f1e9949ef20a2d5014baf638099bbb1
柳 耕太 <yanagikouta0602@gmail.com>
2016-06-10 10:15:49 +0900

    no message

--- routes/index.router.php
+++ routes/index.router.php
@@ -8,7 +8,7 @@ $app->get('/', function(){
 
 $app->get('/callback', function(){
     file_put_contents("/tmp/kouta.txt", "get");
-    // Line::api_send_line(Config::read('line.send_id'), "aaaa");
+    Line::api_send_line(Config::read('line.send_id'), "aaaa");
 });
 
 $app->post('/callback', function(){
@@ -20,6 +20,6 @@ $app->post('/callback', function(){
     $from = $content->from;
     $message_id = $content->id;
     $content_type = $content->contentType;
-    
+
     Line::api_send_line(Config::read('line.send_id'), "aaaa");
 });

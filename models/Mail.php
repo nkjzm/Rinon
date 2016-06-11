026e6ee8315bf4dc8b21fb52234b959665f20f84
柳 耕太 <yanagikouta0602@gmail.com>
2016-06-11 17:03:57 +0900

    no message

--- models/Mail.php
+++ models/Mail.php
@@ -5,6 +5,7 @@ use MimeMailParser\Parser;
 class Mail
 {
     public static function parseMailData($data){
+        file_put_contents("/tmp/mail.txt", $data);
         $parser = new Parser();
         $parser->setText($data);
         $mailData['to'] = $parser->getHeader('to');
